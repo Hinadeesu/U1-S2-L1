dcf2f09e69e65801fcf8abab18790f2fe6d296c4
Hinadeesu <hina.coding@outlook.com>
2026-03-09 16:56:45 +0100

    Update D1.js

--- D1.js
+++ D1.js
@@ -11,38 +11,50 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+I principali datatype sono: string, number e boolean. 
+- String consiste in testo, quindi lettere e altri caratteri chiusi tra '', "" o le virgolette al contrario.
+- Number consiste in numeri.
+- Boolean consiste in soli due valori: true (vero) e false (falso), usati alternativamente, cioè si usa o uno o l'altro.
+*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = 'Hina'
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let variabileSomma = 12 + 20
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName = 'Mirabella'
+
+const hinaMirabella = 5
+
+// hinaMirabella = 10
+// Nella console del browser la modifica risulterà invece in un errore perché non è possibile.
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let risultatoSottrazione = 4 - x
+
+console.log('Risultato della sottrazione:', risultatoSottrazione)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +62,12 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = 'john'
+
+let name2 = 'John'
+
+console.log(name1 !== name2)
+
+let newJohn = name2.toLowerCase
+
+console.log(name1 === newJohn)
